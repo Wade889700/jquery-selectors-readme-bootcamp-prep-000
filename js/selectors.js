4c98aabf8bbd28473bd3104a1566b3cc55d28e09
severus889700 <severus889700@gmail.com>
2018-10-09 06:02:09 +0000

    automatically backed up by learn

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,12 @@
 // declare your functions here...
+function paragraphSelector(){
+  return $('p');
+}
+
+function LastImageSelector(){
+  return $('img:last')
+}
+
+function ninjaBabySelector(){
+  return $("#ninja-baby")
+}
